f9ec5882c88f85605c6c7d300fbbcbdff5e38a97
Zach Saunders <zachisaunders12@gmail.com>
2024-07-28 11:07:08 -0400

    Added first and second loops

--- src/main.rs
+++ src/main.rs
@@ -16,15 +16,32 @@ impl Solution {
         l1: Option<Box<ListNode>>,
         l2: Option<Box<ListNode>>,
     ) -> Option<Box<ListNode>> {
-        let mut l1 = l1;
-        let mut l2 = l2;
-        let sum: i32 = 0;
-        let first_num: i32 = 0;
-        let second_num: i32 = 0;
+        let mut l1 = l1.unwrap();
+        let mut l2 = l2.unwrap();
+        let mut sum: i32 = 0;
+        let mut first_num: i32 = 0;
+        let mut second_num: i32 = 0;
 
-        loop {
-            let node = match l1 {};
+        let mut i: u32 = 0;
+        'first_loop: loop {
+            first_num += l1.val * 10_i32.pow(i);
+            i += 1;
+            l1 = match l1.next {
+                Some(node) => node,
+                None => break 'first_loop,
+            }
         }
+
+        i = 0;
+        'second_loop: loop {
+             += l1.val * 10_i32.pow(i);
+            i += 1;
+            l1 = match l1.next {
+                Some(node) => node,
+                None => break 'second_loop,
+            }
+        }
+        return None;
     }
 }
 
