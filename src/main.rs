050dac1b9880a7bd12990e1e84626cea7838f33e
Zach Saunders <zachisaunders12@gmail.com>
2024-07-28 22:37:26 -0400

    Okay, I'm starting to roll.

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use std::collections::HashMap;
+
+struct Solution {
+    _placeholder: i32
+}
+
+impl Solution {
+    pub fn length_of_longest_substring(s: String) -> i32 {
+        let mut char_dict = HashMap::new().reserve(s.len());
+        let mut longest_substr: u32 = 0;
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+}
