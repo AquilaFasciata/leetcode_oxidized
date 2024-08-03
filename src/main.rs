6771c5b15e691ed9697f6b2139d7306e1f80c5ac
Zach Saunders <zachisaunders12@gmail.com>
2024-08-03 16:06:23 -0400

    Got most of it working, odd edge case that I'm not 100% sure why it's not working yet.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,34 @@
 use std::collections::HashMap;
 
 struct Solution {
-    _placeholder: i32
+    _placeholder: i32,
 }
 
 impl Solution {
     pub fn length_of_longest_substring(s: String) -> i32 {
-        let mut char_dict = HashMap::new().reserve(s.len());
-        let mut longest_substr: u32 = 0;
+        let mut char_dict: HashMap<char, u32> = HashMap::new();
+        let mut longest_substr: i32 = 0;
+        let mut temp_substr: i32 = 0;
+
+        let s = s.chars();
+        for letter in s {
+            if char_dict.contains_key(&letter) {
+                char_dict.clear();
+                if temp_substr > longest_substr {
+                    longest_substr = temp_substr;
+                }
+                temp_substr = 0;
+            } else {
+                char_dict.insert(letter, 1);
+                temp_substr += 1;
+            }
+        }
+        return longest_substr;
     }
 }
 
 fn main() {
+    let out = Solution::length_of_longest_substring(String::from("pwwkew"));
+    println!("{out}");
     println!("Hello, world!");
 }
