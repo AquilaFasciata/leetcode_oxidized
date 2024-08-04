7b5dbdc6dd9d26c5bb7357420673d80c76fdbb04
Zach Saunders <zachisaunders12@gmail.com>
2024-08-03 23:14:47 -0400

    Want to rewrite; original idea was flawed

--- src/main.rs
+++ src/main.rs
@@ -5,31 +5,7 @@ struct Solution {
 }
 
 impl Solution {
-    pub fn length_of_longest_substring(s: String) -> i32 {
-        let mut char_dict: HashMap<char, u32> = HashMap::new();
-        let mut longest_substr: i32 = 0;
-        let mut temp_substr: i32 = 0;
-
-        let s = s.chars();
-        for letter in s {
-            if char_dict.contains_key(&letter) {
-                char_dict.clear();
-                char_dict.insert(letter, 1);
-                if temp_substr > longest_substr {
-                    longest_substr = temp_substr;
-                }
-                temp_substr = 1;
-            } else {
-                char_dict.insert(letter, 1);
-                temp_substr += 1;
-            }
-        }
-
-        if temp_substr > longest_substr {
-            return temp_substr;
-        }
-        return longest_substr;
-    }
+    pub fn length_of_longest_substring(s: String) -> i32 {}
 }
 
 fn main() {
