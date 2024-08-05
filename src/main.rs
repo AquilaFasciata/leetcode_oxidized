5d3842e7dcc102d146316b146d3edaa7701c3904
Zach Saunders <zachisaunders12@gmail.com>
2024-08-05 19:00:17 -0400

    Used a tutorial for this :(

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,29 @@
-use std::collections::HashMap;
+use std::{cmp::max, collections::HashSet};
 
 struct Solution {
     _placeholder: i32,
 }
 
 impl Solution {
-    pub fn length_of_longest_substring(s: String) -> i32 {}
+    pub fn length_of_longest_substring(s: String) -> i32 {
+        let mut char_set: HashSet<char> = HashSet::new();
+        let mut left_ind = 0;
+        //right_ind is the letter.0 in the enumerated for loop
+        let mut result = 0;
+
+        for letter in s.clone().chars().enumerate() {
+            loop {
+                if !char_set.insert(letter.1) {
+                    char_set.remove(&s.chars().nth(left_ind as usize).unwrap());
+                    left_ind += 1;
+                } else {
+                    break;
+                }
+            }
+            result = max(result, (letter.0 as i32) - left_ind + 1);
+        }
+        return result;
+    }
 }
 
 fn main() {
