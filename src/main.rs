d913e6967cdbfc1e202ed2dfad226b5fe93f0a63
Zach <zachisaunders12@gmail.com>
2024-08-06 14:28:43 -0400

    Basic logic done

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,25 @@ struct Solution {
 
 impl Solution {
     pub fn minimum_pushes(word: String) -> i32 {
+        let mut result: i32 = 0;
         let mut weight_map: HashMap<char, i32> = HashMap::with_capacity(26);
         for letter in word.chars() {
             let letter_weight = weight_map.entry(letter).or_insert(0);
             *letter_weight += 1;
         }
 
-        return 0_i32;
+        // TODO Sort weight map
+
+        let mut keypad: Vec<Vec<char>> = vec![Vec::new(); 8];
+
+        for letter in weight_map {
+            let key_ind: usize = letter.1 as usize % keypad.len();
+            let key = keypad.get_mut(key_ind).unwrap();
+            key.push(letter.0);
+            result += key.len() as i32;
+        }
+
+        return result;
     }
 }
 
